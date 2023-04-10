15d057ffbdff839012d2d4d11bf2dab12ef4024b
Nick Garbalau <garbalaunick@gmail.com>
2023-04-10 18:45:52 +0300

    Initial commit with some basic loops

---
+++ src/routes/+page.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+  const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
+  const operations = ['/', 'x', '+', '-', '='];
+</script>
+
+<main>
+  <div class='calculator'>
+    <div class='results'></div>
+    <div class='digits'>
+      <div class='numbers'>
+        <button class="btn btn-xlg btn-silver">C</button>
+        {#each numbers as number}
+          <button class={`btn ${number === '0' ? 'btn-lg' : null}`}>
+            {number}
+          </button>
+        {/each}
+      </div>
+      <div class='operations'>
+        {#each operations as operation}
+          <button class="btn btn-orange">{operation}</button>
+        {/each}
+    </div>
+  </div>
+</main>
+
+
+<style>
+  main {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .calculator {
+    background-color: rgb(28, 28, 28);
+    width: 240px;
+    padding: 15px;
+    border-radius: 7px;
+  }
+  .results {
+    height: 60px;
+    color: white;
+    font-size: 50px;
+    display: flex;
+    flex-direction: row-reverse;
+    margin-right: 10px;
+  }
+  .digits {
+    display: flex;
+  }
+  .numbers {
+    display: flex;
+    flex-wrap: wrap;
+    width: 200px;
+  }
+  .operations {
+    display: flex;
+    flex-direction: column;
+  }
+  .btn {
+    width: 50px;
+    height: 50px;
+    border-radius: 100px;
+    background-color: rgb(114, 113, 113);
+    font-size: 20px;
+    font-weight: bold;
+    color: white;
+    margin: 5px;
+    border: none;
+  }
+  .btn-lg {
+    width: 110px;
+  }
+  .btn-orange {
+    background-color: orange;
+  }
+  .btn-silver {
+    background-color: silver;
+  }
+  .btn-xlg {
+    width: 170px;
+  }
+</style>
